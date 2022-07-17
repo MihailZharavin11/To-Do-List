8dafcfb88a7d6f15e2ed034ab85961e938248fa5
Mikhail <mihail.zharavin65@gmail.com>
2022-07-17 20:47:50 +0300

    Added module and reset style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
+import styles from './main.module.scss';
 
-function App() {
+const App = () => {
   return <div className="App">Test sss</div>;
-}
+};
 
 export default App;
